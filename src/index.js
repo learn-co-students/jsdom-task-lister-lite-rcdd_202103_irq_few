bdc5ba8d9f7390b9aee095ed026a59a9820d6270
eman <emanhharki@gmail.com>
2021-04-13 00:25:55 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+
+// });
+const inputfield = document.getElementById("new-task-description");
+const submit =document.getElementById("btn");
+
+submit.addEventListener("submit",form);
+function form(){
+const ul= document.getElementById("tasks");
+const li = document.createElement("li");
+ul.append(li);
+
+const form2= document.createElement("form");
+const button =document.createElement("button");
+button.innerHTML("X");
+form2.append(button);
+
+ li.append(form2);
+
+}
