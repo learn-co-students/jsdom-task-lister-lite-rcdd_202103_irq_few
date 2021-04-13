c2ef0e4359675b994dfb872bb487b00c1f6b7553
Abdulmajeed Jaafer <abdulmajeedjaafer76@gmail.com>
2021-04-13 02:02:05 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const inputText = document.getElementById('new-task-description');
+  const form = document.getElementById('create-task-form');
+  const listDiv = document.getElementById('list');
+  const taskUL = document.getElementById('tasks')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let inputTextValue = inputText.value;
+    let li = document.createElement('li');
+    li.innerText = inputTextValue;
+    taskUL.append(li);
+  });
 });
